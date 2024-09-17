d53582b76146a3f763fa9513638a1bdb409c3ee0
palegavr <palegavr@gmail.com>
2024-09-17 20:25:19 +0300

    Выполнена задача backend`a #1

---
+++ CarShop/CarShop.ServiceDefaults/CommonTypes/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShop.ServiceDefaults.CommonTypes
+{
+    public class Car
+    {
+        public long Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public double PriceForStandardConfiguration { get; set; }
+        public string Color { get; set; }
+        public double EngineCapacity { get; set; }
+        public CorpusType CorpusType { get; set; }
+        public FuelType FuelType { get; set; }
+        public int Count { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
--- CarShop/CarShop.Web/Controllers/CatalogController.cs
+++ CarShop/CarShop.Web/Controllers/CatalogController.cs
@@ -5,10 +5,39 @@ namespace CarShop.Web.Controllers
 {
     public class CatalogController : Controller
     {
+        private List<Car> _cars = new List<Car>()
+        {
+            new Car { Id = 1, Brand = "Brand1", Model = "Model1",
+                PriceForStandardConfiguration = 100,
+                Color = "Красный",
+                EngineCapacity = 30,
+                CorpusType = CorpusType.Sedan,
+                FuelType = FuelType.Petrol,
+                Count = 123,
+                ImageUrl = "https://img1.akspic.ru/previews/3/8/0/8/7/178083/178083-gonochnyj_avtomobil-legkovyye_avtomobili-avtomobilnoe_osveshhenie-koleso-purpurnyj_cvet-x750.jpg" },
+
+            new Car { Id = 2, Brand = "Brand2", Model = "Model2",
+                PriceForStandardConfiguration = 200,
+                Color = "Желтый",
+                EngineCapacity = 40,
+                CorpusType = CorpusType.Sedan,
+                FuelType = FuelType.Diesel,
+                Count = 456,
+                ImageUrl = "https://img2.akspic.ru/previews/2/8/1/8/7/178182/178182-legkovyye_avtomobili-sportkar-lambordzhini-superkar-lamborgini_aventador-x750.jpg" },
+            new Car { Id = 3, Brand = "Brand3", Model = "Model3",
+                PriceForStandardConfiguration = 300,
+                Color = "Зеленый",
+                EngineCapacity = 50,
+                CorpusType = CorpusType.Hatchback,
+                FuelType = FuelType.Diesel | FuelType.Electric,
+                Count = 987,
+                ImageUrl = "https://img3.akspic.ru/previews/1/7/8/7/7/177871/177871-zelenyj_lamborgini-lamborghini_gallardo-lambordzhini-lamborghini_urakan-legkovyye_avtomobili-x750.jpg" }
+        };
+
         [HttpGet]
         public IActionResult Index()
         {
-            throw new NotImplementedException();
+            ViewData["Cars"] = _cars;
             return View();
         }
 
