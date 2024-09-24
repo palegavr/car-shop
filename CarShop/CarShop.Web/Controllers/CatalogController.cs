8a5c33f6319ec6080f79842621d45644099fc1d4
palegavr <palegavr@gmail.com>
2024-09-24 17:58:20 +0300

    При переходе на /catalog/{id}, при отсутствии указанного id в базе данных, отображается страница ошибки 404

--- CarShop/CarShop.Web/Controllers/CatalogController.cs
+++ CarShop/CarShop.Web/Controllers/CatalogController.cs
@@ -7,35 +7,6 @@ namespace CarShop.Web.Controllers
     [Route("[controller]/{id?}")]
     public class CatalogController(CarStorageClient _carStorageClient) : Controller
     {
-        private List<Car> _cars = new List<Car>()
-        {
-            new Car { Id = 1, Brand = "Brand1", Model = "Model1",
-                PriceForStandardConfiguration = 100,
-                Color = "Красный",
-                EngineCapacity = 30,
-                CorpusType = CorpusType.Sedan,
-                FuelType = FuelType.Petrol,
-                Count = 123,
-                ImageUrl = "https://img1.akspic.ru/previews/3/8/0/8/7/178083/178083-gonochnyj_avtomobil-legkovyye_avtomobili-avtomobilnoe_osveshhenie-koleso-purpurnyj_cvet-x750.jpg" },
-
-            new Car { Id = 2, Brand = "Brand2", Model = "Model2",
-                PriceForStandardConfiguration = 200,
-                Color = "Желтый",
-                EngineCapacity = 40,
-                CorpusType = CorpusType.Sedan,
-                FuelType = FuelType.Diesel,
-                Count = 456,
-                ImageUrl = "https://img2.akspic.ru/previews/2/8/1/8/7/178182/178182-legkovyye_avtomobili-sportkar-lambordzhini-superkar-lamborgini_aventador-x750.jpg" },
-            new Car { Id = 3, Brand = "Brand3", Model = "Model3",
-                PriceForStandardConfiguration = 300,
-                Color = "Зеленый",
-                EngineCapacity = 50,
-                CorpusType = CorpusType.Hatchback,
-                FuelType = FuelType.Diesel | FuelType.Electric,
-                Count = 987,
-                ImageUrl = "https://img3.akspic.ru/previews/1/7/8/7/7/177871/177871-zelenyj_lamborgini-lamborghini_gallardo-lambordzhini-lamborghini_urakan-legkovyye_avtomobili-x750.jpg" }
-        };
-
         [HttpGet]
         public async Task<IActionResult> Index([FromRoute] long? id)
         {
@@ -62,8 +33,14 @@ namespace CarShop.Web.Controllers
 
         public async Task<IActionResult> IdIndexAsync(long id)
         {
-            ViewData["Car"] = await _carStorageClient.GetCarAsync(id);
-            return View("~/Views/Catalog/id/Index.cshtml");
+            Car? car = await _carStorageClient.GetCarAsync(id);
+
+            if (car is null)
+            {
+                return NotFound();
+            }
+
+			return View("~/Views/Catalog/id/Index.cshtml", car);
         }
     }
 }
