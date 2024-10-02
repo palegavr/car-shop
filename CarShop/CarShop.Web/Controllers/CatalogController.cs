7da2a7a22adbfa0da7cb7e2d833ff6220045c299
palegavr <palegavr@gmail.com>
2024-10-02 20:34:25 +0300

    При поиске на /catalog данные передаются методом GET (раньше был POST)

--- CarShop/CarShop.Web/Controllers/CatalogController.cs
+++ CarShop/CarShop.Web/Controllers/CatalogController.cs
@@ -13,81 +13,61 @@ namespace CarShop.Web.Controllers
         private const int CARS_COUNT_ON_ONE_PAGE = 30;
 
         [HttpGet]
-        public async Task<IActionResult> Index([FromRoute] long? id, [FromQuery(Name = "page")] int page = 1)
+        public async Task<IActionResult> Index(
+            [FromRoute] long? id,
+			[FromQuery(Name = "brand")] string? brand,
+			[FromQuery(Name = "minimum_engine_capacity")] double? minimumEngineCapacity,
+			[FromQuery(Name = "maximum_engine_capacity")] double? maximumEngineCapacity,
+			[FromQuery(Name = "fuel_type")] FuelType? fuelType,
+			[FromQuery(Name = "corpus_type")] CorpusType? corpusType,
+			[FromQuery(Name = "minimum_price")] double? minimumPrice,
+			[FromQuery(Name = "maximum_price")] double? maximumPrice,
+			[FromQuery(Name = "page")] int? page)
         {
             if (id is not null) { return await IdIndexAsync(id.Value); }
+            
+            page ??= 1;
+            if (page <= 0) 
+                page = 1;
 
-            GetCarsResult getCarsResult = await _carStorageClient.GetCarsAsync();
-
-            IEnumerable<Car> cars = getCarsResult.Cars
-                .Skip(CARS_COUNT_ON_ONE_PAGE * (page - 1))
-                .Take(CARS_COUNT_ON_ONE_PAGE);
-
-            int pagesCount = getCarsResult.TotalResultsCount / CARS_COUNT_ON_ONE_PAGE;
-
-            if (getCarsResult.TotalResultsCount % CARS_COUNT_ON_ONE_PAGE > 0)
-                pagesCount++;
-
-			CatalogViewModel viewModel = new CatalogViewModel 
-            { 
-                Cars = cars,
-                CurrentPage = page,
-                PagesCount = pagesCount
-			};
-            return View(viewModel);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Index(
-			[FromForm(Name = "start_index")] int? startIndex,
-			[FromForm(Name = "end_index")] int? endIndex,
-			[FromForm(Name = "brand")] string? brand,
-            [FromForm(Name = "minimum_engine_capacity")] double? minimumEngineCapacity,
-            [FromForm(Name = "maximum_engine_capacity")] double? maximumEngineCapacity,
-            [FromForm(Name = "fuel_type")] FuelType? fuelType,
-            [FromForm(Name = "corpus_type")] CorpusType? corpus_type,
-            [FromForm(Name = "minimum_price")] double? minimumPrice,
-            [FromForm(Name = "maximum_price")] double? maximumPrice,
-            [FromForm(Name = "sort_by")] SortBy? sortBy,
-            [FromForm(Name = "sort_type")] SortType? sortType,
-			[FromForm(Name = "page")] int page = 1
-			)
-        {
 			GetCarsOptions getCarsOptions = new GetCarsOptions
-            {
-                StartIndex = startIndex,
-                EndIndex = endIndex,
-                Brand = brand,
-                MinimumEngineCapacity = minimumEngineCapacity,
-                MaximumEngineCapacity = maximumEngineCapacity,
-                FuelType = fuelType,
-                CorpusType = corpus_type,
-                MinimumPrice = minimumPrice,
-                MaximumPrice = maximumPrice,
-                SortBy = sortBy,
-                SortType = sortType
-            };
+			{
+				Brand = brand,
+				MinimumEngineCapacity = minimumEngineCapacity,
+				MaximumEngineCapacity = maximumEngineCapacity,
+				FuelType = fuelType,
+				CorpusType = corpusType,
+				MinimumPrice = minimumPrice,
+				MaximumPrice = maximumPrice
+			};
 
 			GetCarsResult getCarsResult = await _carStorageClient.GetCarsAsync(getCarsOptions);
 
-			IEnumerable<Car> cars = getCarsResult.Cars
-				.Skip(CARS_COUNT_ON_ONE_PAGE * (page - 1))
-				.Take(CARS_COUNT_ON_ONE_PAGE);
+            IEnumerable<Car> cars = getCarsResult.Cars
+                .Skip(CARS_COUNT_ON_ONE_PAGE * (page.Value - 1))
+                .Take(CARS_COUNT_ON_ONE_PAGE);
 
-			int pagesCount = getCarsResult.TotalResultsCount / CARS_COUNT_ON_ONE_PAGE;
+            int pagesCount = getCarsResult.TotalResultsCount / CARS_COUNT_ON_ONE_PAGE;
+            if (getCarsResult.TotalResultsCount % CARS_COUNT_ON_ONE_PAGE > 0)
+                pagesCount++;
 
-			if (getCarsResult.TotalResultsCount % CARS_COUNT_ON_ONE_PAGE > 0)
-				pagesCount++;
+            bool containsSearchParameters =
+                brand is not null ||
+                minimumEngineCapacity is not null ||
+                maximumEngineCapacity is not null ||
+                fuelType is not null ||
+                corpusType is not null ||
+                minimumPrice is not null ||
+                maximumPrice is not null;
 
-			CatalogViewModel viewModel = new CatalogViewModel
-			{
-				Cars = cars,
-				CurrentPage = page,
-				PagesCount = pagesCount,
-				IsSearchResultsPage = true
-			};
-
-			return View(viewModel);
+            CatalogViewModel viewModel = new CatalogViewModel
+            {
+                Cars = cars,
+                CurrentPage = page.Value,
+                PagesCount = pagesCount,
+                IsSearchResultsPage = containsSearchParameters
+            };
+            return View(viewModel);
         }
 
         public async Task<IActionResult> IdIndexAsync(long id)
