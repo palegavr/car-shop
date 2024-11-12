f61f770ce442c333f3eac1512fff37ff27cc3008
palegavr <palegavr@gmail.com>
2024-11-12 13:07:25 +0200

    Сделана страница /admin/account/{id}

--- CarShop/web-frontend/src/app/layout.tsx
+++ CarShop/web-frontend/src/app/layout.tsx
@@ -13,11 +13,13 @@ export default function RootLayout({
     return (
         <html lang="en">
         <head>
+            <meta name="viewport" content="initial-scale=1, width=device-width"/>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"/>
         </head>
         <body>
-        <Nav/>
-        {children}
+        <div>
+            {children}
+        </div>
         <script type={'text/javascript'} src={'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js'}/>
         <script type={'text/javascript'}
                 src={'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'}/>
--- CarShop/web-frontend/src/clients/backendСlient.tsx
+++ CarShop/web-frontend/src/clients/backendСlient.tsx
@@ -1,6 +1,42 @@
 import {ProcessData} from "@/types/types";
 import {encodeProcessDataToJson} from "@/utilities/processDataOperations";
 
+enum AccountAction {
+    Create,
+    ChangePassword,
+    Ban,
+    Unban,
+    GiveRole,
+    TakeRole,
+    SetPriority
+}
+
+type AccountActionPayload = {
+    action: AccountAction,
+    data?: {
+        email?: string,
+        password?: string,
+        oldPassword?: string,
+        role?: string,
+        priority?: number
+    }
+}
+
+export const ExistingRoles = [
+    'admin.car.add',
+    'admin.car.edit',
+    'admin.car.delete',
+    'admin.account.create',
+    'admin.account.change-password.own',
+    'admin.account.change-password.other',
+    'admin.account.ban.own',
+    'admin.account.ban.other',
+    'admin.account.unban',
+    'admin.account.role.give',
+    'admin.account.role.take',
+    'admin.account.priority.set'
+] as const;
+export type Role = typeof ExistingRoles[number];
 
 type UploadImageResult = {
     success: boolean,
@@ -59,5 +95,162 @@ export async function pushProcessData(carId: number, processData: ProcessData): 
         return {success: response.ok}
     } catch (error) {
         return {success: false}
+    }
+}
+
+export type SetPriorityResult = {
+    success: boolean,
+}
+export async function setPriorityAsync(accountId: number, priority: number): Promise<SetPriorityResult> {
+    try {
+        const payload: AccountActionPayload = {
+            action: AccountAction.SetPriority,
+            data: {
+                priority: priority
+            }
+        };
+
+        const response = await fetch(`/api/admin/account/${accountId}`, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        });
+
+        return {success: response.ok}
+    } catch (error) {
+        return {success: false}
+    }
+}
+
+export type BanResult = {
+    success: boolean,
+}
+export async function banAsync(accountId: number): Promise<BanResult> {
+    try {
+        const payload: AccountActionPayload = {
+            action: AccountAction.Ban,
+        };
+        const response = await fetch(`/api/admin/account/${accountId}`, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        });
+
+        return {success: response.ok}
+    } catch (error) {
+        return {success: false}
+    }
+}
+export type UnbanResult = {
+    success: boolean,
+}
+export async function unbanAsync(accountId: number): Promise<UnbanResult> {
+    try {
+        const payload: AccountActionPayload = {
+            action: AccountAction.Unban,
+        };
+        const response = await fetch(`/api/admin/account/${accountId}`, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        });
+
+        return {success: response.ok}
+    } catch (error) {
+        return {success: false}
+    }
+}
+
+export type GiveRoleResult = {
+    success: boolean,
+}
+export async function giveRoleAsync(accountId: number, role: Role): Promise<GiveRoleResult> {
+    try {
+        const payload: AccountActionPayload = {
+            action: AccountAction.GiveRole,
+            data: {
+                role: role
+            }
+        };
+        const response = await fetch(`/api/admin/account/${accountId}`, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        });
+
+        return {success: response.ok}
+    } catch (error) {
+        return {success: false}
+    }
+}
+
+export type TakeRoleResult = {
+    success: boolean,
+}
+export async function takeRoleAsync(accountId: number, role: Role): Promise<TakeRoleResult> {
+    try {
+        const payload: AccountActionPayload = {
+            action: AccountAction.TakeRole,
+            data: {
+                role: role
+            }
+        };
+        const response = await fetch(`/api/admin/account/${accountId}`, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        });
+
+        return {success: response.ok}
+    } catch (error) {
+        return {success: false}
+    }
+}
+
+export type ChangePasswordResult = {
+    success: boolean,
+    oldPasswordIncorrect?: boolean
+}
+export async function changePasswordAsync(accountId: number, newPassword: string, oldPassword: string): Promise<ChangePasswordResult> {
+    try {
+        const payload: AccountActionPayload = {
+            action: AccountAction.ChangePassword,
+            data: {
+                password: newPassword,
+                oldPassword: oldPassword
+            }
+        };
+        const response = await fetch(`/api/admin/account/${accountId}`, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        });
+
+        return {
+            success: response.ok,
+            oldPasswordIncorrect: response.ok ? undefined : response.status === 400
+        }
+    } catch (error) {
+        return {
+            success: false
+        }
     }
 }
