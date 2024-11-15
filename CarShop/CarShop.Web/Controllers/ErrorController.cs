d0a0cd0ed92feb018498a720bd90ba79bf8bc875
palegavr <palegavr@gmail.com>
2024-11-15 22:15:14 +0200

    Сделана страница ошибки 400 и всех непредвиденных ошибок

--- CarShop/CarShop.Web/Controllers/ErrorController.cs
+++ CarShop/CarShop.Web/Controllers/ErrorController.cs
@@ -7,17 +7,34 @@ namespace CarShop.Web.Controllers
 	{
 		public IActionResult Index([FromRoute] int code)
 		{
-			if (code == StatusCodes.Status404NotFound)
-				return NotFound404();
-			else if (code == StatusCodes.Status401Unauthorized)
-				return Redirect("/admin/login");
-
-			return View();
+			return code switch
+			{
+				StatusCodes.Status404NotFound => NotFound404(),
+				StatusCodes.Status500InternalServerError => InternalError500(),
+				StatusCodes.Status401Unauthorized => Unauthorized401(),
+				StatusCodes.Status400BadRequest => BadRequest400(),
+				_ => View(),
+			};
 		}
 
 		private IActionResult NotFound404() 
 		{
 			return View("NotFound404");
+		}
+		
+		private IActionResult Unauthorized401() 
+		{
+			return Redirect("/admin/login");
+		}
+		
+		private IActionResult BadRequest400()
+		{
+			return View("BadRequest400");
+		}
+
+		private IActionResult InternalError500()
+		{
+			return View("InternalError500");
 		}
 	}
 }
